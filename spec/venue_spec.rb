c6723972360f2189c26db434f9ce6d314c3166f1
Michael Dao <mike@turing.io>
2021-03-23 14:17:42 -0600

    removes each hook

--- spec/venue_spec.rb
+++ spec/venue_spec.rb
@@ -2,27 +2,28 @@ require 'rspec'
 require './lib/venue'
 
 describe Venue do
-  let(:venue) { Venue.new('Bluebird', 4) }
-  # Iteration 1
-
   describe '#initialize' do
     it 'is a venue' do
       skip
+      venue = Venue.new('Bluebird', 4)
       expect(venue).to be_a Venue
     end
 
     it 'can read the name' do
       skip
+      venue = Venue.new('Bluebird', 4)
       expect(venue.name).to eq 'Bluebird'
     end
 
     it 'can read the capacity' do
       skip
+      venue = Venue.new('Bluebird', 4)
       expect(venue.capacity).to eq 4
     end
 
     it 'has no patrons by default' do
       skip
+      venue = Venue.new('Bluebird', 4)
       expect(venue.patrons).to eq []
     end
   end
@@ -32,6 +33,7 @@ describe Venue do
   describe '#add_patron' do
     it 'returns a list of patrons' do
       skip
+      venue = Venue.new('Bluebird', 4)
       venue.add_patron('Mike')
       venue.add_patron('Megan')
       venue.add_patron('Bob')
@@ -43,6 +45,7 @@ describe Venue do
   describe '#yell_at_patrons' do
     it 'returns a list of uppercased names' do
       skip
+      venue = Venue.new('Bluebird', 4)
       venue.add_patron('Mike')
       venue.add_patron('Megan')
       venue.add_patron('Bob')
