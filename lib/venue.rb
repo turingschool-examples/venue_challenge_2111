8b81d4fbfd2478020f295b20e637a5201666a8c8
Arnaldo Aparicio <apaarn2211@gmail.com>
2021-10-13 12:59:23 -0400

    Add variables and methods

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,28 @@
 class Venue
+  attr_reader :name, :capacity, :patrons
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @patrons = []
+  end
+
+  def add_patron(add)
+    @patrons << add
+  end
+
+  def yell_at_patrons
+    @patrons.map {|p| p.upcase}
+  end
+
+  def over_capacity?
+    if @patrons.count > @capacity
+      true
+    else
+      false
+    end
+  end
+  def kick_out
+    @patrons.shift
+  end
+
 end
