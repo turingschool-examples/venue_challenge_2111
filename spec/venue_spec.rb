8923bf4358141512a854d87ba41dfcd8c0c9b234
Arnaldo Aparicio <apaarn2211@gmail.com>
2021-10-13 13:00:17 -0400

    Add iteration 3

--- spec/venue_spec.rb
+++ spec/venue_spec.rb
@@ -4,25 +4,21 @@ require './lib/venue'
 describe Venue do
   describe '#initialize' do
     it 'is a venue' do
-      skip
       venue = Venue.new('Bluebird', 4)
       expect(venue).to be_a Venue
     end
 
     it 'can read the name' do
-      skip
       venue = Venue.new('Bluebird', 4)
       expect(venue.name).to eq 'Bluebird'
     end
 
     it 'can read the capacity' do
-      skip
       venue = Venue.new('Bluebird', 4)
       expect(venue.capacity).to eq 4
     end
 
     it 'has no patrons by default' do
-      skip
       venue = Venue.new('Bluebird', 4)
       expect(venue.patrons).to eq []
     end
@@ -32,24 +28,54 @@ describe Venue do
 
   describe '#add_patron' do
     it 'returns a list of patrons' do
-      skip
       venue = Venue.new('Bluebird', 4)
       venue.add_patron('Mike')
       venue.add_patron('Megan')
       venue.add_patron('Bob')
-
       expect(venue.patrons).to eq ['Mike', 'Megan', 'Bob']
     end
   end
 
   describe '#yell_at_patrons' do
     it 'returns a list of uppercased names' do
-      skip
       venue = Venue.new('Bluebird', 4)
       venue.add_patron('Mike')
       venue.add_patron('Megan')
       venue.add_patron('Bob')
       expect(venue.yell_at_patrons).to eq ['MIKE', 'MEGAN', 'BOB']
+    end
+  end
+
+
+
+#Iteration 3
+
+  describe '#over_capacity' do
+    it 'states if venue is over capacity' do
+      venue = Venue.new('Bluebird', 4)
+      venue.add_patron('Mike')
+      venue.add_patron('Megan')
+      venue.add_patron('Bob')
+      venue.add_patron('James')
+      venue.add_patron('Cat')
+      expect(venue.patrons).to eq ['Mike', 'Megan', 'Bob', 'James', 'Cat']
+      expect(venue.over_capacity?).to eq(true)
+    end
+  end
+
+  # Utilizes 'shift' method to kick out
+  # so it will remove the first element,
+  # which would be 'Mike'
+  describe '#kick_out' do
+    it 'kicks patrons out' do
+      venue = Venue.new('Bluebird', 4)
+      venue.add_patron('Mike')
+      venue.add_patron('Megan')
+      venue.add_patron('Bob')
+      venue.add_patron('James')
+      venue.add_patron('Cat')
+      expect(venue.patrons).to eq ['Mike', 'Megan', 'Bob', 'James', 'Cat']
+      expect(venue.kick_out).to eq 'Mike'
     end
   end
 end
