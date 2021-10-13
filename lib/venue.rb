f5128b6f3e12ad8739f279c3fb2e8b7b96243c30
Hannah Warren <hannahkwarren@gmail.com>
2021-10-13 13:05:17 -0400

    Add Iterations 1 through 4

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,37 @@
 class Venue
+
+  attr_reader :name, :capacity, :patrons
+
+  def initialize(name, capacity, patrons = [])
+    @name = name
+    @capacity = capacity
+    @patrons = patrons
+  end
+
+  def add_patron(name)
+    @patrons.push(name)
+  end
+
+  def yell_at_patrons
+    yelling = []
+    patrons.each do |yell|
+      yelling << yell.upcase
+    end
+    yelling
+  end
+
+  def over_capacity?
+    patrons.length > capacity
+  end
+
+  def kick_out
+    surplus = patrons.length - capacity
+    if surplus > 0
+      patrons.pop(surplus)
+    else
+      return patrons
+    end
+    patrons
+  end
+
 end
