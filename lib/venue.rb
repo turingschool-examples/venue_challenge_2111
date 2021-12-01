1d655ecaf0870d77a58ea2f9f3e023bda169a143
Nick Cooley <cooley8706@gmail.com>
2021-12-01 10:37:10 -0700

    Last commit

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,6 +1,18 @@
 class Venue
-  def initialize(venue, integer)
+  def initialize(venue, capacity)
     @venue = venue
-    @integer = integer
+    @capacity = capacity
+    end
+
+  def can_read_the_name(name, capacity)
+    @venue.name = bluebird
+end
+
+  def can_read_the_capacity(venue, capacity)
+    @venue.capacity = 4
   end
+
+  def has_no_patrons_by_default(venue, patrons)
+    @venue.patrons = []
+end
 end
