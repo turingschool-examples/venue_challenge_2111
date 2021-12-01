77e54055674ae3519ea6ed038efbd9fe022af0ca
oscar santos <oscar303data@gmail.com>
2021-12-01 09:44:57 -0700

    over capacity block was created
    following the interaction pattern over capacity assertion was created.

--- spec/venue_spec.rb
+++ spec/venue_spec.rb
@@ -39,12 +39,22 @@ describe Venue do
 
   describe '#yell_at_patrons' do
     it 'returns a list of uppercased names' do
-      
       venue = Venue.new('Bluebird', 4)
       venue.add_patron('Mike')
       venue.add_patron('Megan')
       venue.add_patron('Bob')
       expect(venue.yell_at_patrons).to eq ['MIKE', 'MEGAN', 'BOB']
+    end
+
+    it 'check if it is over capacity' do
+      venue = Venue.new('Bluebird', 4)
+      venue.add_patron('Mike')
+      venue.add_patron('Megan')
+      venue.add_patron('Bob')
+      expect(venue.over_capacity?).to eq(false)
+      venue.add_patron('James')
+      venue.add_patron('Cat')
+      expect(venue.over_capacity?).to eq(true)
     end
   end
 end
