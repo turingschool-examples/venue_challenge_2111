9b1c6c828d96a798cdb1c7ddc0b4f7432911abb2
Joseph Galvin <jwgalvin@gmail.com>
2021-12-01 09:38:43 -0700

    add empty array to intialize, add add_patron, pass test, remove skip in spec file

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,11 +1,17 @@
 class Venue
   attr_reader :name, :capacity
+
   def initialize(name, capacity)
     @name = name
     @capacity = capacity
+    @patrons = []
   end
+
   def patrons
-    patrons = []
+    @patrons
   end
-  
+
+  def add_patron(string)
+    @patrons << string
+  end
 end
