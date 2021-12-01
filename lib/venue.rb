c21134c8b2fd6d919a0937b1e3cd3998e6afe05a
Austin Moore <austinchristianmoore@gmail.com>
2021-12-01 11:30:17 -0500

    Passed venue name test

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,7 +1,11 @@
 #require 'pry'
 class Venue
+  attr_reader :name, :capacity, :patrons
+
   def initialize(name, capacity)
-    @name = name
-    @capacity = capacity
+    #binding.pry
+    @name      = name
+    @capacity  = capacity
+    @patrons   =[]
   end
 end
