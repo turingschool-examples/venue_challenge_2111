0a4309c4fe3d065b70be3051aab765ee5c1340e6
Maximilian Wagner <maw3350@gmail.com>
2021-12-01 11:35:54 -0500

    Add attr_reader to pass test 2

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,6 +1,9 @@
 class Venue
-  def initialize(argument, arg2)
-    @argument = argument
+  attr_reader :name, :arg2
+  def initialize(name, arg2)
+    @name = name
     @arg2     = arg2
   end
+
+
 end
