0dfbc4989877f1b6f4f91aebe878ec8f0a14756e
Michael Brandt <msbrandt00@gmail.com>
2021-12-01 09:23:01 -0700

    Add iterations 1 and 2

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,17 @@
 class Venue
+
+  attr_reader :name, :capacity, :patrons
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @patrons = []
+  end
+
+  def add_patron(person)
+    @patrons.push person
+  end
+
+  def yell_at_patrons
+    @patrons.map {|patron| patron.upcase}
+  end
 end
