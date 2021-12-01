80ebe8da27329e502440d54314fffbe6a2755b84
Maximilian Wagner <maw3350@gmail.com>
2021-12-01 11:41:11 -0500

    Add capacity attribute

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,8 +1,8 @@
 class Venue
-  attr_reader :name, :arg2
-  def initialize(name, arg2)
-    @name = name
-    @arg2     = arg2
+  attr_reader :name, :capacity
+  def initialize(name, capacity)
+    @name     = name
+    @capacity = capacity
   end
 
 
