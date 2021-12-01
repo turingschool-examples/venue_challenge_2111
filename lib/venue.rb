93c56caaec1657434d9760ed2e79a6fc636f09fe
Adrian Campos <adriancampos2992@yahoo.com>
2021-12-01 08:42:06 -0800

    pass venue 1-4 iteration

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,16 @@
 class Venue
+  attr_reader :patrons
+  def initialize(name, capacity)
+    @name     = name
+    @capacity = 4
+    @patrons  = []
+  end
+
+  def name
+  @name
+  end
+
+  def capacity
+  @capacity
+  end
 end
