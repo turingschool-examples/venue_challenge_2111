7c91f847f475464e0a42b5c19ba8fbe9c7f1584b
Maximilian Wagner <maw3350@gmail.com>
2021-12-01 12:38:29 -0500

    Attempt to create yell_at_patrons methods

--- spec/venue_spec.rb
+++ spec/venue_spec.rb
@@ -22,7 +22,7 @@ describe Venue do
     end
 
     it 'has no patrons by default' do
-      
+
       venue = Venue.new('Bluebird', 4)
       expect(venue.patrons).to eq []
     end
@@ -32,7 +32,7 @@ describe Venue do
 
   describe '#add_patron' do
     it 'returns a list of patrons' do
-      skip
+
       venue = Venue.new('Bluebird', 4)
       venue.add_patron('Mike')
       venue.add_patron('Megan')
@@ -44,7 +44,7 @@ describe Venue do
 
   describe '#yell_at_patrons' do
     it 'returns a list of uppercased names' do
-      skip
+
       venue = Venue.new('Bluebird', 4)
       venue.add_patron('Mike')
       venue.add_patron('Megan')
